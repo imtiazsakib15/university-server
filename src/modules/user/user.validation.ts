e42a3813eb559caf72498f47ec7b014478438c36
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2024-06-08 20:22:35 +0600

    user validation schema updated

--- src/modules/user/user.validation.ts
+++ src/modules/user/user.validation.ts
@@ -1,16 +1,14 @@
 import { z } from 'zod';
 
 const userValidationSchema = z.object({
-  id: z.string(),
   password: z
-    .string()
+    .string({
+      invalid_type_error: 'Password must be a string',
+    })
     .min(6, { message: 'Password must be at least 6 characters long' })
     .max(20, { message: 'Password must be less than 20 characters' })
-    .regex(/[0-9]/, { message: 'Password must contain at least one digit' }),
-  needsPasswordChange: z.boolean().default(true),
-  role: z.enum(['admin', 'student', 'faculty']),
-  status: z.enum(['in-progress', 'blocked']).default('in-progress'),
-  isDeleted: z.boolean().default(false),
+    .regex(/[0-9]/, { message: 'Password must contain at least one digit' })
+    .optional(),
 });
 
 export { userValidationSchema };
