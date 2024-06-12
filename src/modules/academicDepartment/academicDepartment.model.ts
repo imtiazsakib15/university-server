ab36d388f09ea5579468bb347ee93b9211da6392
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2024-06-12 13:11:10 +0600

    timestamps added in academic faculty and academic department

--- src/modules/academicDepartment/academicDepartment.model.ts
+++ src/modules/academicDepartment/academicDepartment.model.ts
@@ -1,18 +1,23 @@
 import { Schema, model } from 'mongoose';
 import { IAcademicDepartment } from './academicDepartment.interface';
 
-const academicDepartmentSchema = new Schema<IAcademicDepartment>({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
+const academicDepartmentSchema = new Schema<IAcademicDepartment>(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    academicFaculty: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'AcademicFaculty',
+    },
   },
-  academicFaculty: {
-    type: Schema.Types.ObjectId,
-    required: true,
-    ref: 'AcademicFaculty',
+  {
+    timestamps: true,
   },
-});
+);
 
 export const AcademicDepartment = model<IAcademicDepartment>(
   'AcademicDepartment',
--- src/modules/academicFaculty/academicFaculty.model.ts
+++ src/modules/academicFaculty/academicFaculty.model.ts
@@ -1,13 +1,18 @@
 import { Schema, model } from 'mongoose';
 import { IAcademicFaculty } from './academicFaculty.interface';
 
-const academicFacultySchema = new Schema<IAcademicFaculty>({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
+const academicFacultySchema = new Schema<IAcademicFaculty>(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
   },
-});
+  {
+    timestamps: true,
+  },
+);
 
 export const AcademicFaculty = model<IAcademicFaculty>(
   'AcademicFaculty',
