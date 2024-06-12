cfa52873e7ff7870afa0c095470a21cf599e5e35
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2024-06-12 09:39:18 +0600

    academic faculty interface and model created

---
+++ src/modules/academicFaculty/academicFaculty.model.ts
@@ -0,0 +1,15 @@
+import { Schema, model } from 'mongoose';
+import { IAcademicFaculty } from './academicFaculty.interface';
+
+const academicFacultySchema = new Schema<IAcademicFaculty>({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+export const AcademicFaculty = model<IAcademicFaculty>(
+  'AcademicFaculty',
+  academicFacultySchema,
+);
