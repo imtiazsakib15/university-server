c456f3a91b761966ef30d2d4b7158839c26083dd
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2024-06-12 12:16:20 +0600

    academic department interface and model created

---
+++ src/modules/academicDepartment/academicDepartment.model.ts
@@ -0,0 +1,20 @@
+import { Schema, model } from 'mongoose';
+import { IAcademicDepartment } from './academicDepartment.interface';
+
+const academicDepartmentSchema = new Schema<IAcademicDepartment>({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  academicFaculty: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'AcademicFaculty',
+  },
+});
+
+export const AcademicDepartment = model<IAcademicDepartment>(
+  'AcademicDepartment',
+  academicDepartmentSchema,
+);
