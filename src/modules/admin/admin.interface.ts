a9a71d171701a5b2f8cda8cb37b44c37aa84c97b
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2024-08-20 16:14:54 +0600

    admin interface created

---
+++ src/modules/admin/admin.interface.ts
@@ -0,0 +1,18 @@
+import { Types } from 'mongoose';
+import { IUserName } from '../student/student.interface';
+
+export interface IAdmin {
+  id: string;
+  user: Types.ObjectId;
+  name: IUserName;
+  gender: 'male' | 'female';
+  dateOfBirth?: string;
+  email: string;
+  designation: string;
+  contactNo: string;
+  presentAddress: string;
+  permanentAddress: string;
+  profileImg: string;
+  manageDepartment: Types.ObjectId;
+  isDeleted: boolean;
+}
