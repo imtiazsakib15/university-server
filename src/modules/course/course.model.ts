800644bb9f7cc357ffd21265262780dd9a92bdd9
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2024-08-21 18:19:36 +0600

    course model added

---
+++ src/modules/course/course.model.ts
@@ -0,0 +1,55 @@
+import { model, Schema } from 'mongoose';
+import { ICourse, IPreRequisiteCourse } from './course.interface';
+
+const preRequisiteCourseSchema = new Schema<IPreRequisiteCourse>(
+  {
+    course: {
+      type: Schema.Types.ObjectId,
+      required: [true, 'Please provide the course ID for the prerequisite.'],
+      ref: 'Course',
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    _id: false,
+  },
+);
+
+const courseSchema = new Schema<ICourse>(
+  {
+    title: {
+      type: String,
+      required: [true, 'Please provide the course title.'],
+      unique: true,
+      trim: true,
+    },
+    prefix: {
+      type: String,
+      required: [true, 'Please provide the course prefix.'],
+      trim: true,
+    },
+    code: {
+      type: String,
+      required: [true, 'Please provide the course code.'],
+      unique: true,
+      trim: true,
+    },
+    credit: {
+      type: String,
+      required: [true, 'Please provide the course credit.'],
+      trim: true,
+    },
+    preRequisiteCourse: {
+      type: [preRequisiteCourseSchema],
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const Course = model<ICourse>('Course', courseSchema);
