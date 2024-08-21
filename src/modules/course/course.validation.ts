2b227cbc6afecc3535bd655e7fe1115461b3eeb2
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2024-08-22 02:38:14 +0600

    update course validation schema created

--- src/modules/course/course.validation.ts
+++ src/modules/course/course.validation.ts
@@ -29,4 +29,37 @@ const createSchema: AnyZodObject = z.object({
   }),
 });
 
-export const CourseSchemas = { createSchema };
+const updatePreRequisiteCourseSchema: AnyZodObject = z.object({
+  course: z.string({ required_error: 'Course ID is required.' }).optional(),
+  isDeleted: z.boolean().optional(),
+});
+
+const updateSchema: AnyZodObject = z.object({
+  body: z.object({
+    course: z.object({
+      title: z
+        .string({ required_error: 'Course title is required.' })
+        .min(1, 'Course title cannot be empty.')
+        .trim()
+        .optional(),
+      prefix: z
+        .string({ required_error: 'Course prefix is required.' })
+        .min(1, 'Course prefix cannot be empty.')
+        .trim()
+        .optional(),
+      code: z
+        .number({ required_error: 'Course code is required.' })
+        .positive('Course code must be a positive number.')
+        .int('Course code must be an integer.')
+        .optional(),
+      credit: z
+        .number({ required_error: 'Course credit is required.' })
+        .positive('Course credit must be a positive number.')
+        .int('Course credit must be an integer.')
+        .optional(),
+      preRequisiteCourse: z.array(updatePreRequisiteCourseSchema).optional(),
+    }),
+  }),
+});
+
+export const CourseValidationSchemas = { createSchema, updateSchema };
